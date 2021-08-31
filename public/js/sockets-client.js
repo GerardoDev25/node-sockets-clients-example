c33e73a4942ae4a88c0407e0df6f26c7846eb528
GerardoDev25 <GerardoDev25@gmail.com>
2021-08-30 20:12:35 -0400

    7. Mensajes de conexión y desconexión - Cliente

--- public/js/sockets-client.js
+++ public/js/sockets-client.js
@@ -1,1 +1,19 @@
-const socket = io()
+const ele = (el) => document.querySelector(el);
+
+const $online = ele("#online");
+const $offline = ele("#offline");
+
+$online.style.display = "none";
+$offline.style.display = "none";
+
+const socket = io();
+
+socket.on("connect", () => {
+   $online.style.display = "inline";
+   $offline.style.display = "none";
+});
+
+socket.on("disconnect", () => {
+   $online.style.display = "none";
+   $offline.style.display = "inline";
+});
